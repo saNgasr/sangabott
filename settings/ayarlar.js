da502509c59e354fe7cc417554e0fa8f87754088
saNgasr <151746086+saNgasr@users.noreply.github.com>
2023-11-22 16:09:24 +0300

    Add files via upload

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTA2Mzc3MjA1MTY5OTE1MDg0OA.GxEIyP._Aj6tKLCg2JdG1igDZPH71pYXRJSmxUNbhyYYA",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "!", // bot prefix
+    SAHİP: process.env.SAHİP || "687002582849159171", //sahip id :()
+}
